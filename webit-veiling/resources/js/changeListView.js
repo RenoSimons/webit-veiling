8a4295865e362d28d1de86bb4a0dd2eed2355d8f
reno simons <simons.reno@skynet.be>
2021-10-11 17:34:23 +0200

    Added toggle list view in dashboard

---
+++ webit-veiling/resources/js/changeListView.js
@@ -0,0 +1,18 @@
+$('.change-view-icon').click(function(){
+    // Show the right icon
+    $('#large, #small').toggleClass('d-none');
+
+    if (this.id === "small") {
+        $('.dashboard-list-item').removeClass('d-md-flex').addClass('w-45')
+        $('.toggable-list').addClass('d-flex flex-wrap justify-content-between')
+
+        // Make content stretch over card
+        $('.dashboard-list-item').children().removeClass('col-md-4')
+    } else {
+        // Get back to the original state
+        $('.dashboard-list-item').addClass('d-md-flex').removeClass('w-45')
+        $('.toggable-list').removeClass('d-flex flex-wrap justify-content-between')
+
+        $('.dashboard-list-item').children().addClass('col-md-4')
+    }
+});
--- webit-veiling/resources/views/admin/dashboard.blade.php
+++ webit-veiling/resources/views/admin/dashboard.blade.php
@@ -4,9 +4,13 @@
 <div class="container">
     <h1>Dashboard</h1>
     <!-- Add new product button -->
-    <div class="d-md-flex justify-content-between w-100">
-        <div class="my-3">
+    <div class="w-100">
+        <div class="my-3 d-md-flex justify-content-between">
             <button class="button-main" type="button" data-toggle="modal" data-target="#add-product-modal">Add new product</button>
+            <div class="d-none d-md-flex">
+                <img class="change-view-icon d-none" id="large" src="images/large.png" alt="large-view">
+                <img class="change-view-icon" id="small" src="images/small.png" alt="small-view">
+            </div>
         </div>
     </div>
 
@@ -16,28 +20,32 @@
     <!-- Overview of all products list -->
     <div class="my-5">
         <h2>Products in offering</h2>
-        <ul class="list-group">
+        <ul class="toggable-list p-0">
             @if(count($data) > 0)
             @foreach($data as $product)
-            <li class="list-item shadow d-md-flex justify-content-around align-items-center my-3">
-                <img src="{{ 'storage/product_images/' .$product->img_url }}" alt="product-image" class="img-fluid d-md-w-25">
-                <div>
-                    <h4>{{ $product->name }}</h4>
-                    <p>€{{ $product->start_price }}</p>
-                    <p>Hoogste bod: €{{ $product->highest_offer }}</p>
-                    <p class="small">Online since: {{ $product->close_date }}</p>
-                </div>
+            <li class="dashboard-list-item custom-card d-md-flex justify-content-around align-items-center my-3 p-2">
+                    <div class="col-md-4 d-flex justify-content-center">
+                        <img src="{{ 'storage/product_images/' .$product->img_url }}" alt="product-image" class="img-fluid d-md-w-25">
+                    </div>
+                    
+                    <div class="col-md-4 text-center">
+                        <h4>{{ $product->name }}</h4>
+                        <p>€{{ $product->start_price }}</p>
+                        <p>Hoogste bod: €{{ $product->highest_offer }}</p>
+                        <p class="small">Online since: {{ $product->close_date }}</p>
+                    </div>
 
-                <div class="d-flex">
-                <a class="btn btn-warning mr-2" href="{{ route('products.show', $product) }}">Show</a>
-                    <button data-toggle="modal" data-target="#edit-product-modal{{$product->id}}" class="btn btn-info">Edit</button>
-                    <x-edit-product-modal :data="$product" />
-                    <form action="{{ route('products.destroy', $product->id) }}" method="post">
-                        @csrf
-                        {{ method_field('DELETE') }}
-                        <button type="submit" class="btn btn-danger ml-2">Remove</button>
-                    </form>
-                </div>
+                    <div class="d-flex justify-content-center col-md-4">
+                        <a class="btn btn-warning mr-2" href="{{ route('products.show', $product) }}">Show</a>
+                        <button data-toggle="modal" data-target="#edit-product-modal{{$product->id}}" class="btn btn-info">Edit</button>
+                        <x-edit-product-modal :data="$product" />
+                        <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                            @csrf
+                            {{ method_field('DELETE') }}
+                            <button type="submit" class="btn btn-danger ml-2">Remove</button>
+                        </form>
+                    </div>
+             
             </li>
             @endforeach
             @else
